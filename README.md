8d3ce86fe91a16648f209a003bc5b2aee01d28c6
GlaDos <glados28@yandex.ru>
2017-01-30 06:54:24 +0300

    minor update; read README for changes

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # "Mere" - library for JavaScript
 
-Library that allows to bind functions to the string-formed tasks (JavaScript's Strings) and then execute function through its related task.
+Library that allows to bind functions to the string-formed tasks (JavaScript's Strings) and then execute functions through their related tasks.
+Together with this, a collection of associated tools is attached. 
 
 ### Installation
 
@@ -10,24 +11,107 @@ npm i mere --save
 
 ### Usage
 
-Bind a function to the string-formed task:
+In our context tasks are mappings { string : function } or their derivatives.
+The world "derivative" means all task combinations that can be achived through using such methods as
+    "task".task
+    "task".with(...)
+    "task".then(...)
+
+##### Bind a function to the string-formed task
 
 ```javascript
 "task name".bind((arg1, arg2, ...) => {
 	/* some logic */
 });
+
+"task name".bind(task); /* bind from another task  */
 ```
 
-Execute a function:
+Also `.bind()` returns a string that was binded to the function.
+
+##### Require mere library
+
+```javascript
+require("mere");
+```
+
+##### Execute a function
 
 ```javascript
 const res = "task name".make(arg1, arg2, ...); /* may throw Errors! */
 ```
 
-Return a function wrapped in promise:
+##### Return a function wrapped in promise
 
 ```javascript
 const promise = "task name".promise(arg1, arg2, ...); /* errors will call the promise's reject branch! */
+```
+
+##### Return a task with inlined first n arguments
+
+```javascript
+const newTask = "task name".with(arg1, arg2, ..., argn); /* return a task with last input arguments starts from n+1 */
+```
+
+##### Return a task that will call another task after (put the first result as the first argument of the second one, if result !== undefined)
+
+```javascript
+const newTask = "first task name".then("second task name", arg1_or_2, arg2_or_3, ...); /* return a chained task with no input arguments for the first one */
+```
+
+i.e `newTask` takes arguments for the first task and after takes the result (if exist) and args in `.then()` for the second task.
+
+##### Put one task as the argument for another one
+
+```javascript
+const res = "task with one arg".make("second task name".task); /* if second task has no arguments */
+
+const res = "task with one arg".make("second task name".with(arg1, arg2, ...)); /* otherwise */
+```
+
+##### Try interesting combinations
+
+```javascript
+const promise = "first task name".then("second task name", secondArgs...).with(firstArgs...).promise(lastargs...);
+```
+
+That one is stupid, but You will do it better!
+
+##### Task arrays also want to be in the topic!
+
+```javascript
+["task name 1", "task name 2", "task name 3"].make();
+["task name 1", "task name 2", "task name 3"].promise();
+
+/* as fact, it works the same as the next code: */
+"task name 1".then("task name 2").then("task name 3").make();
+"task name 1".then("task name 2").then("task name 3").promise();
+
+/* task arrays as arguments */
+"first task name".make(["second task name", "third task name"].task);
+
+/* and even task arrays in task arrays! */
+["task name 1", ["task name 2"], "task name 3"].make();
+```
+
+##### Additional options
+
+```javascript
+const mere = require("mere");
+
+console.log(mere.getArgCheck()); /* return 1 === mere.NOT_MORE */
+
+/* if argument number must equal with the formal argument number (or an Error occured) */
+mere.setArgCheck(mere.MUST_EQUAL);
+
+/* if argument number must not be more than the formal argument number (or an Error occured) */
+mere.setArgCheck(mere.NOT_MORE); /* the default setting */
+
+/* if argument number must not be less than the formal argument number (or an Error occured) */
+mere.setArgCheck(mere.NOT_LESS);
+
+/* if We don't want to think about argument number mismatch */
+mere.setArgCheck(mere.NO_CHECK);
 ```
 
 ### Examples
@@ -49,9 +133,7 @@ require("mere");
 ```javascript
 require("mere");
 
-"sum".bind((num1, num2) => {
-	return num1 + num2;
-});
+"sum".bind((num1, num2) => num1 + num2);
 
 console.log("sum".make(2, 3));
 
@@ -72,12 +154,78 @@ require("mere");
 "fib".bind((k) => {
 	if (k <= 2)
 		return 1;
-	else
-		return "fib".make(k - 1) + "fib".make(k - 2);
+	
+	return "fib".make(k - 1) + "fib".make(k - 2);
 });
 
 console.log("fib".make(5));
 ```
+
+##### Sum and call
+
+```javascript
+require("mere");
+
+"sum"  .bind((num1, num2) => num1 + num2);
+"print".bind((msg) => {
+	console.log(msg);
+});
+
+"sum".then("print").make(2, 2);
+```
+
+##### Number doubler
+
+```javascript
+require("mere");
+
+"mult"  .bind((num1, num2) => num1 * num2);
+"double".bind("mult".with(2));
+
+console.log("double".make(2));
+```
+
+##### Faggot thrower
+require("mere");
+
+```javascript
+
+"print".bind((msg) => {
+	console.log(msg);
+});
+
+"who I am".bind(() => "you are a faggot");
+
+"print".make("who I am".task);
+/* or what is equally (almost: first one will execute the task later in some cases): */
+"print".make("who I am".make());
+/* or even this way */
+"who I am".then("print").make();
+```
+
+##### Some arithmetic
+
+```javascript
+require("mere");
+
+"sum" .bind((num1, num2) => num1 + num2);
+"mult".bind((num1, num2) => num1 * num2);
+
+console.log(["sum", "mult".with(2)].make(2, 2)); /* I hope You know that is eight */
+```
+
+##### Don't do this things
+
+```javascript
+require("mere");
+
+"lol".bind(() => "lol".make());
+"lol".task.task.with().with().task.task;
+[[], [].task, [].with()].make();
+
+```
+
+Because of its inanity.
 
 ### Future releases
 
--- index.js
+++ index.js
@@ -8,45 +8,235 @@
 "use strict";
 
 /**
- * Constants.
+ * Map {string} task : {function} module.
  */
 const taskModuleMap = {};
 
-String.prototype.bind = function (func) {
-	taskModuleMap[this] = func;
+/**
+ * Library configuration.
+ */
+const config = {
+	lessArgAllowed : true,
+	moreArgAllowed : false
 };
 
-String.prototype.make = function (...args) {
-	const func = taskModuleMap[this];
-
-	if (typeof func !== "function")
-		throw new Error(`task ${this} is not binded to the function`);
-
-	if (func.length >= args.length)
-		return func(...args);
-
-	throw new Error(`too many arguments: given ${args.length}, expected ${func.length}`);
+const ARG_CHECK_ENUM = {
+	NO_CHECK   : 0,
+	NOT_MORE   : 1,
+	NOT_LESS   : 2,
+	MUST_EQUAL : 3
 };
 
-String.prototype.promise = function (...args) {
-	const func = taskModuleMap[this];
+//** utility functions
 
-	if (typeof func !== "function")
-		throw new Error(`task ${this} is not binded to the function`);
+const execFunc = (func, ...args) => {
+	const trueArgs = [];
 
-	if (func.length >= args.length)
+	for (let i = 0; i < args.length; i += 1)
+		if (args[i] && args[i].constructor && args[i].constructor.name === "MereTask")
+			trueArgs.push(args[i].make());
+		else
+			trueArgs.push(args[i]);
+
+	return typeof func === "function" ? func(...trueArgs) : func.func(...trueArgs);
+};
+
+const getFAN = (func) => /* get formal argument number */
+	typeof func === "function" ? func.length : func.mereFAN;
+
+const ensureArgNum = (given, expected) => {
+	if (!config.lessArgAllowed && given < expected)
+		throw new Error(`too few arguments: given ${given}, expected ${expected}`);
+
+	if (!config.moreArgAllowed && given > expected)
+		throw new Error(`too more arguments: given ${given}, expected ${expected}`);
+};
+
+//**
+
+class MereTask {
+	constructor (func) {
+		this.task = this;
+		this.func = func;
+	}
+
+	make (...args) {
+		if (typeof this.func !== "function" && (!this.func || typeof this.func.mereFAN !== "number"))
+			throw new Error(`task ${this} is not binded to the function`);
+
+		ensureArgNum(args.length, getFAN(this.func));
+
+		return execFunc(this.func, ...args);
+	}
+
+	promise (...args) {
+		if (typeof this.func !== "function" && (!this.func || typeof this.func.mereFAN !== "number"))
+			throw new Error(`task ${this} is not binded to the function`);
+
+		ensureArgNum(args.length, getFAN(this.func));
+
 		return new Promise((resolve, reject) => {
 			try {
-				resolve(func(...args));
+				resolve(execFunc(this.func, ...args));
 			} catch (err) {
 				reject(err);
 			}
 		});
+	}
 
-	throw new Error(`too many arguments: given ${args.length}, expected ${func.length}`);
+	with (...args) {
+		const formalArgNum = getFAN(this.func);
+
+		if (!config.moreArgAllowed && args.length > formalArgNum)
+			throw new Error(`too more arguments: given ${args.length}, expected ${formalArgNum}`);
+
+		return new MereTask({
+			mereFAN : formalArgNum - args.length,
+			func : (...lastArgs) => {
+				return execFunc(this.func, ...args, ...lastArgs);
+			}
+		});
+	}
+
+	then (task, ...secondArgs) {
+		if (typeof task === "string")
+			task = task.task;
+
+		if (task && task.constructor && task.constructor.name === "Array")
+			task = getArrayTask(task);
+
+		if (!task || !task.constructor || task.constructor.name !== "MereTask")
+			throw new Error(`task expected, got ${task}`);
+
+		ensureArgNum(secondArgs.length, getFAN(task.func));
+
+		return new MereTask({
+			mereFAN : getFAN(this.func), /* to save the formal argument number */
+			func    : (...args) => {
+				const firstRes = execFunc(this.func, ...args);
+
+				if (firstRes === undefined)
+					return execFunc(task.func, ...secondArgs);
+
+				return execFunc(task.func, firstRes, ...secondArgs);
+			}
+		});
+	}
+}
+
+String.prototype.bind = function (func) {
+	if (func && func.constructor) {
+		if (func.constructor.name === "MereTask")
+			taskModuleMap[this] = func; /* func === task */
+
+		if (func.constructor.name === "Array")
+			taskModuleMap[this] = getArrayTask(func, "binding"); /* func === task array */
+		else if (typeof func === "function")
+			taskModuleMap[this] = new MereTask(func);
+	} else
+		throw new Error("task must be binded to the function or to another task");
+
+	return this;
+};
+
+String.prototype.__defineGetter__("task", function () {
+	return taskModuleMap[this];
+});
+
+String.prototype.make = function (...args) {
+	return taskModuleMap[this].make(...args);
+};
+
+String.prototype.promise = function (...args) {
+	return taskModuleMap[this].promise(...args);
+};
+
+String.prototype.with = function (...args) {
+	return taskModuleMap[this].with(...args);
+};
+
+String.prototype.then = function (task, ...args) {
+	return taskModuleMap[this].then(task, ...args);
+};
+
+//** arrays
+
+const getArrayTask = (arr, errFuncName) => {
+	if (arr.length === 0)
+		return new MereTask(() => {});
+
+	let finalTask = typeof arr[0] === "string" ? taskModuleMap[arr[0]] : arr[0];
+
+	if (finalTask && finalTask.constructor && finalTask.constructor.name === "Array")
+		finalTask = getArrayTask(finalTask);
+
+	if (!finalTask || !finalTask.constructor || finalTask.constructor.name !== "MereTask")
+		throw new Error(`array must contain only tasks to call ${errFuncName}`);
+
+	for (let i = 1; i < arr.length; i += 1) {
+		if (typeof arr[i] !== "string" && (!arr[i] || !arr[i].constructor || arr[i].constructor.name !== "MereTask"))
+			throw new Error(`array must contain only tasks to call ${errFuncName}`);
+
+		finalTask = finalTask.then(arr[i]);
+	}
+
+	return finalTask;
+};
+
+Array.prototype.__defineGetter__("task", function () {
+	return getArrayTask(this, "to the task property");
+});
+
+Array.prototype.make = function (...args) {
+	if (this.length !== 0)
+		return getArrayTask(this, "make()").promise(...args);
+};
+
+Array.prototype.promise = function (...args) {
+	if (this.length !== 0)
+		return getArrayTask(this, "promise()").promise(...args);
+
+	return new Promise((resolve) => {
+		resolve();
+	});
+};
+
+Array.prototype.with = function (...args) {
+	return getArrayTask(this, "with()").with(...args);
+};
+
+Array.prototype.then = function (task, ...args) {
+	return getArrayTask(this, "then()").then(task, ...args);
 };
 
 /**
  * Exports.
  */
-exports = module.exports = {};
+exports = module.exports = {
+	NO_CHECK       : ARG_CHECK_ENUM.NO_CHECK,
+	NOT_MORE       : ARG_CHECK_ENUM.NOT_MORE,
+	NOT_LESS       : ARG_CHECK_ENUM.NOT_LESS,
+	MUST_EQUAL     : ARG_CHECK_ENUM.MUST_EQUAL,
+	getArgCheck    : () => {
+		if (config.lessArgAllowed && config.moreArgAllowed)
+			return ARG_CHECK_ENUM.NO_CHECK;
+
+		if (config.lessArgAllowed)
+			return ARG_CHECK_ENUM.NOT_MORE;
+
+		if (config.moreArgAllowed)
+			return ARG_CHECK_ENUM.NOT_LESS;
+
+		return ARG_CHECK_ENUM.MUST_EQUAL;
+	},
+	setArgCheck    : (type) => {
+		if (type > 3)
+			throw new Error(`invalid argument checking type: ${type} (use mere.ARG_CHECK_ENUM)`);
+
+		if (type < 2)
+			config.lessArgAllowed = true;
+
+		if (type === 0)
+			config.moreArgAllowed = true;
+	}
+};
